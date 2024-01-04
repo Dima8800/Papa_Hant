b4beb0dc16d3f2e4e049dfe55ccbbbba5f19222e
dima8800 <dmitry2006happy@gmail.com>
2024-01-04 18:28:35 +0500

    fourth day finish

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,12 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
+      orders: [],
       items: [
         {
           id: 1,
           title: 'Кошелек c одним карманом',
-          img: './',
+          img: 'first-kosh.jpg',
           desc: 'Натуральная кожа1',
           category: 'кошелек',
           price: '5000'
@@ -19,7 +20,7 @@ class App extends React.Component {
         {
           id: 2,
           title: 'Кошелек с двумя карманами',
-          img: '',
+          img: 'second-kosh.jpg',
           desc: 'Натуральная кожа2',
           category: 'кошелек',
           price: '6000'
@@ -27,22 +28,35 @@ class App extends React.Component {
         {
           id: 3,
           title: 'сумка для мужчины',
-          img: '',
+          img: 'first-symka.jpg',
           desc: 'Натуральная кожа3',
           category: 'сумка',
           price: '5000'
         }
       ]
     }
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
   }
+
   render() {
     return (
       <div className="wrapper">
-        <Header/>
-        <Items items = {this.state.items}/>
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <Items items = {this.state.items} onAdd={this.addToOrder} />
         <Footer/>
       </div>
     )
+  }
+
+  deleteOrder(id){
+    console.log(id)
+  }
+
+  addToOrder(item){
+    this.setState({orders: [...this.state.orders, item]}, () => {
+      console.log(this.state.orders)
+    })
   }
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaCartArrowDown } from "react-icons/fa";
+import Order from './Order';
 
-export default function header() {
+const showOrders = (props) => {
+  return (<div>
+    {props.orders.map(el => (
+      <Order onDelete={props.onDelete} key={el.id} item={el} />
+    ))}
+  </div>
+  )
+}
+
+const showNothing = () => {
+  return (
+    <div className='empty'>
+      <h2>
+        Товаров нет
+      </h2>
+    </div>
+  )
+}
+
+export default function Header(props) {
+  let [cartOpen, setCartOpen] = useState(false)
+
   return (
     <header>
         <div>
@@ -10,6 +33,15 @@ export default function header() {
               <li>Контакты</li>
               <li>Кабинет</li>
             </ul>
+            <FaCartArrowDown onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+            
+            {cartOpen && (
+              <div className='shop-cart'>
+                {props.orders.length > 0 ?
+                showOrders(props) : showNothing()}
+              </div>
+            )}
+        
         </div>
         <div className='presentation'></div>
     </header>
